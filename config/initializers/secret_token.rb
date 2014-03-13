543ce93532e2f7b775473471e404e492afb2920b
henryh28 <henry@henryhsu.net>
2014-03-13 14:33:01 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RabbitOverflow::Application.config.secret_token = 'b7f82c676706ad085ba95137e8a5407ec95a6eab69f325b082f113a5af0817728fb692675e2005878a6818cd07b9472d895b5ea6a92e22351dfb5a94cc891498'
